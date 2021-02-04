8efccef6567c7f8d9ad9e97d380fb39d0f423081
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2021-02-04 21:05:11 +0700

    Update and rename hp_2.js to hp_3.js

--- ha/hp_3.js
+++ ha/hp_3.js
@@ -1,6 +1,5 @@
 /* redS Start */
 location.hash.indexOf("#me")>-1&&(window.location.replace("/p/about-me.html"));
-location.hash.indexOf("#blog")>-1&&(window.location="/p/about-blog.html");
 
 location.hash.indexOf("#fb")>-1&&(window.location="https://facebook.com/hamdaniazzah");
 location.hash.indexOf("#ig")>-1&&(window.location="https://instagram.com/hamdaniazzah");
