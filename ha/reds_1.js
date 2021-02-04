244e3f0965f7ddeb00c469168bebd6a77bd23e6e
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2021-02-04 20:34:20 +0700

    Create reds_1.js

---
+++ ha/reds_1.js
@@ -0,0 +1,7 @@
+/* Pages */
+location.hash.indexOf("#me")>-1&&(window.location="/p/about-me.html");
+location.hash.indexOf("#blog")>-1&&(window.location="/p/about-blog.html");
+
+/* Social */
+location.hash.indexOf("#trakteer")>-1&&(window.location="https://trakteer.id/hamdaniazzah");
+location.hash.indexOf("#ig")>-1&&(window.location="https://instagram.com/hamdaniazzah");
