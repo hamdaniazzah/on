889b5e3a0f84ce8eada946c3b7a3f1c9e1948e34
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2021-02-04 20:47:29 +0700

    Update and rename reds_1.js to hp_1.js

--- ha/hp_1.js
+++ ha/hp_1.js
@@ -1,7 +1,7 @@
-/* Pages */
+/* redS Start */
 location.hash.indexOf("#me")>-1&&(window.location="/p/about-me.html");
 location.hash.indexOf("#blog")>-1&&(window.location="/p/about-blog.html");
 
-/* Social */
-location.hash.indexOf("#trakteer")>-1&&(window.location="https://trakteer.id/hamdaniazzah");
+location.hash.indexOf("#fb")>-1&&(window.location="https://facebook.com/hamdaniazzah");
 location.hash.indexOf("#ig")>-1&&(window.location="https://instagram.com/hamdaniazzah");
+/* redS End */
